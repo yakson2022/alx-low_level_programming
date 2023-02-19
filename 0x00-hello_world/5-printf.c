188f6f0b9961db5516771194604034c325e35066
yakson2022 <garubayakubu@gmailcom>
2023-02-19 10:00:29 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main - A program that print a line using printf function
+* Return: 0 (complete)
+*/
+{int main(void)
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

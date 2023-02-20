dd121a0beb697d1a370d548608cf57aa019b8414
yakson2022 <garubayakubu@gmailcom>
2023-02-20 12:00:38 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that print the size of various types on a computer.
+ * return: 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of char:%i Size of an int:%i Size of a long int:%i Size of a long long int:%i Size of a float:%i, 1,4,4,8,4\n");
+	return (0);
+}
